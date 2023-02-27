88ffbeeab676d56fd933e1198f911a318c804af9
Aron <daveavrosia@gmail.com>
2023-02-27 20:42:08 +0800

    Update auth

--- app/middlewares/auth.middleware.ts
+++ app/middlewares/auth.middleware.ts
@@ -4,26 +4,26 @@ import ENV from "../env";
 
 export default async (_req: Request<any>, _res: Response<any>, _next: NextFunction) => {
 
-    const publicRoutes = [{ url: "/api/v1/users", method: "POST" }, { url: "/api/v1/auth", method: "POST" }]
+    // const publicRoutes = [{ url: "/api/v1/users", method: "POST" }, { url: "/api/v1/auth", method: "POST" }]
 
-    if (checkRoutes(publicRoutes, _req.url, _req.method)) {
-        _next();
-        return;
-    }
+    // if (checkRoutes(publicRoutes, _req.url, _req.method)) {
+    //     _next();
+    //     return;
+    // }
 
-    const token = getToken(_req.headers['authorization']);
+    // const token = getToken(_req.headers['authorization']);
 
-    if (!token) {
-        _res.send({ data: [], status: "fail", message: "Token Needed" });
-        return;
-    }
+    // if (!token) {
+    //     _res.send({ data: [], status: "fail", message: "Token Needed" });
+    //     return;
+    // }
 
-    let userId = await getUserId(token);
+    // let userId = await getUserId(token);
 
-    if (!userId) {
-        _res.send({ data: [], status: "fail", message: "Invalid token" });
-        return;
-    }
+    // if (!userId) {
+    //     _res.send({ data: [], status: "fail", message: "Invalid token" });
+    //     return;
+    // }
 
     _next();
 }
