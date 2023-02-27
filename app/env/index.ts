73bfbe4b89019b633e6b72c8f9a1d70e0a8c23c5
Aron <daveavrosia@gmail.com>
2023-02-27 20:41:45 +0800

    Update mongo connection

--- app/env/index.ts
+++ app/env/index.ts
@@ -6,7 +6,7 @@ const ENV = {
   PORT: process.env.PORT || 9000,
   MONGO_CON:
     process.env.MONGO_CON ||
-    "mongodb://localhost:27017/sample?directConnection=true",
+    "mongodb://localhost:27017/redframe?directConnection=true",
   HASH_SALT: Number(process.env.HASH_SALT) || 10,
   JWT_KEY: process.env.JWT_KEY || "secret",
 };
